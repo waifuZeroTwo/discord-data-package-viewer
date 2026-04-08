cb3e9f4c3ef6ec523716aa441deb39dbed86c088
waifuZeroTwo <hoes.romeo@gmail.com>
2026-04-08 06:03:16 -0700

    Initialize Electron window with preload and renderer layout

---
+++ preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('ddpApi', {
+  pickDataPackage: () => ipcRenderer.invoke('dialog:pick-data-package'),
+  getRuntimeVersions: () => ({
+    electron: process.versions.electron,
+    chrome: process.versions.chrome,
+    node: process.versions.node,
+  }),
+})
